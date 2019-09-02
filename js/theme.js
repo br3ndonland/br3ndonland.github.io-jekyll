9669e97387dff827e3ff5fadd27754432448330f
Brendon Smith <br3ndonland@protonmail.com>
2019-09-02 18:27:31 -0400

    Push theme toggle CSS classes into localStorage

--- js/theme.js
+++ js/theme.js
@@ -1,20 +1,33 @@
 // Theme toggle
 try {
-  const themeButton = document.querySelector("#btn-theme-toggle")
-  themeButton.addEventListener("click", () => {
+  const localStorageLoad = () => {
+    if ("localStorage" in window) {
+      // Retrieve CSS classes from localStorage
+      const bodyClasses = localStorage.getItem("bodyClasses")
+      console.log(bodyClasses)
+    }
+  }
+  document.addEventListener("DOMContentLoaded", () => localStorageLoad())
+  const toggleTheme = () => {
     const body = document.querySelector("body")
     body.classList.toggle("theme-toggle")
-    // push the body class list into local storage
+    if ("localStorage" in window) {
+      const bodyClasses = document.querySelector("body").classList
+      localStorage.setItem("bodyClasses", bodyClasses)
+    }
     const udacityLogoRegular = document.querySelector("#udacity-logo")
     const udacityLogoLight = document.querySelector("#udacity-logo-light")
     if (udacityLogoRegular && udacityLogoLight) {
       const udacityLogos = [udacityLogoRegular, udacityLogoLight]
       udacityLogos.forEach(udacityLogo => {
         udacityLogo.classList.toggle("d-none")
-        // push the logo class list into local storage
+        let logoClasses = udacityLogo.classList
+        localStorage.setItem(udacityLogo.id, logoClasses)
       })
     }
-  })
+  }
+  const themeButton = document.querySelector("#btn-theme-toggle")
+  themeButton.addEventListener("click", () => toggleTheme())
 } catch (e) {
   throw Error(e)
 }
