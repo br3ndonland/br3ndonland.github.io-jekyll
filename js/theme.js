9f9e53e47b726b308254fb8583416e6c68213ef0
Brendon Smith <br3ndonland@protonmail.com>
2019-09-02 17:51:55 -0400

    Add error handling to theme toggle JavaScript

--- js/theme.js
+++ js/theme.js
@@ -1,16 +1,20 @@
 // Theme toggle
-const themeButton = document.querySelector("#btn-theme-toggle")
-themeButton.addEventListener("click", () => {
-  const body = document.querySelector("body")
-  body.classList.toggle("theme-toggle")
-  // push the body class list into local storage
-  const udacityLogoRegular = document.querySelector("#udacity-logo")
-  const udacityLogoLight = document.querySelector("#udacity-logo-light")
-  if (udacityLogoRegular && udacityLogoLight) {
-    const udacityLogos = [udacityLogoRegular, udacityLogoLight]
-    udacityLogos.forEach(udacityLogo => {
-      udacityLogo.classList.toggle("d-none")
-      // push the logo class list into local storage
-    })
-  }
-})
+try {
+  const themeButton = document.querySelector("#btn-theme-toggle")
+  themeButton.addEventListener("click", () => {
+    const body = document.querySelector("body")
+    body.classList.toggle("theme-toggle")
+    // push the body class list into local storage
+    const udacityLogoRegular = document.querySelector("#udacity-logo")
+    const udacityLogoLight = document.querySelector("#udacity-logo-light")
+    if (udacityLogoRegular && udacityLogoLight) {
+      const udacityLogos = [udacityLogoRegular, udacityLogoLight]
+      udacityLogos.forEach(udacityLogo => {
+        udacityLogo.classList.toggle("d-none")
+        // push the logo class list into local storage
+      })
+    }
+  })
+} catch (e) {
+  throw Error(e)
+}
