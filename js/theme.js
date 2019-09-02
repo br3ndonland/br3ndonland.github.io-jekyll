f31eb178b27fe0d40f4b52c7478d26bb7b6d6a03
Brendon Smith <br3ndonland@protonmail.com>
2019-09-02 19:04:23 -0400

    Retrieve CSS classes from localStorage

--- js/theme.js
+++ js/theme.js
@@ -1,24 +1,31 @@
 // Theme toggle
 try {
-  const localStorageLoad = () => {
+  const body = document.querySelector("body")
+  const udacityLogoRegular = document.querySelector("#udacity-logo")
+  const udacityLogoLight = document.querySelector("#udacity-logo-light")
+  const udacityLogos = [udacityLogoRegular, udacityLogoLight]
+  const loadTheme = () => {
     if ("localStorage" in window) {
-      // Retrieve CSS classes from localStorage
-      const bodyClasses = localStorage.getItem("bodyClasses")
-      console.log(bodyClasses)
+      const bodyClasses = localStorage.getItem("body")
+      if (bodyClasses) {
+        console.log(`Body classes: ${bodyClasses}`)
+        // TODO: Set CSS classes to values from localStorage
+      }
+      udacityLogos.forEach(udacityLogo => {
+        let logoClasses = localStorage.getItem(udacityLogo.id)
+        console.log(`Classes for element ID ${udacityLogo.id}: ${logoClasses}`)
+        // TODO: Set CSS classes to values from localStorage
+      })
     }
   }
-  document.addEventListener("DOMContentLoaded", () => localStorageLoad())
+  document.addEventListener("DOMContentLoaded", () => loadTheme())
   const toggleTheme = () => {
-    const body = document.querySelector("body")
     body.classList.toggle("theme-toggle")
     if ("localStorage" in window) {
-      const bodyClasses = document.querySelector("body").classList
-      localStorage.setItem("bodyClasses", bodyClasses)
+      const bodyClasses = body.classList
+      localStorage.setItem("body", bodyClasses)
     }
-    const udacityLogoRegular = document.querySelector("#udacity-logo")
-    const udacityLogoLight = document.querySelector("#udacity-logo-light")
     if (udacityLogoRegular && udacityLogoLight) {
-      const udacityLogos = [udacityLogoRegular, udacityLogoLight]
       udacityLogos.forEach(udacityLogo => {
         udacityLogo.classList.toggle("d-none")
         let logoClasses = udacityLogo.classList
