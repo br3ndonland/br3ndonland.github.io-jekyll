7f979bab57a299f05aec62a88a57596c0c6932a7
Brendon Smith <br3ndonland@protonmail.com>
2019-09-07 17:16:04 -0400

    Toggle CSS classes in localStorage

--- js/theme.js
+++ js/theme.js
@@ -28,9 +28,27 @@ try {
     if (udacityLogoRegular && udacityLogoLight) {
       udacityLogos.forEach(udacityLogo => {
         udacityLogo.classList.toggle("d-none")
-        let logoClasses = udacityLogo.classList
-        localStorage.setItem(udacityLogo.id, logoClasses)
       })
+    }
+    let lsLogo = localStorage.getItem("udacity-logo")
+    let lsLogoLight = localStorage.getItem("udacity-logo-light")
+    if (lsLogo) {
+      if (lsLogo === "svg d-none") {
+        localStorage.setItem("udacity-logo", "svg")
+      } else {
+        localStorage.setItem("udacity-logo", "svg d-none")
+      }
+    } else {
+      localStorage.setItem("udacity-logo", "svg d-none")
+    }
+    if (lsLogoLight) {
+      if (lsLogoLight === "svg d-none") {
+        localStorage.setItem("udacity-logo-light", "svg")
+      } else {
+        localStorage.setItem("udacity-logo-light", "svg d-none")
+      }
+    } else {
+      localStorage.setItem("udacity-logo-light", "svg")
     }
   }
   const themeButton = document.querySelector("#btn-theme-toggle")
